c5c5919ec30a4ffa3a2ba4e59205095d75b554d4
ravilella1234 <masters.selenium@gmail.com>
2019-12-06 18:53:21 +0530

    Revert "added third line in demo class"
    This reverts commit 8ad8aa99b732626482e0b10a6d8ab6139493c3a8.

--- FirstProject/src/com/hdfc/loans/carloans/DemoClass.java
+++ FirstProject/src/com/hdfc/loans/carloans/DemoClass.java
@@ -5,7 +5,6 @@ public class DemoClass {
 	public static void main(String[] args) {
 		System.out.println("First Line added....");
 		System.out.println("Second Line added....");
-		System.out.println("Third Line added....");
 
 	}
 
