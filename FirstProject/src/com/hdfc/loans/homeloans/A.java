1f7814b24b878f8202360071e585f1913def3a45
ravilella1234 <masters.selenium@gmail.com>
2019-12-09 18:37:15 +0530

    added add method in class A from california branch

--- FirstProject/src/com/hdfc/loans/homeloans/A.java
+++ FirstProject/src/com/hdfc/loans/homeloans/A.java
@@ -2,9 +2,17 @@ package com.hdfc.loans.homeloans;
 
 public class A 
 {
+	int x=100,y=200,result;
+	
 	public  void m1()
 	{
 		System.out.println("i am m1 from Class A");
+	}
+	
+	public void add()
+	{
+		result=x+y;
+		System.out.println("Addition of X & Y is : " + result);
 	}
 
 	public static void main(String[] args) 
